3601ad02a2591e3e4cbfad1b47eb8681eb6f0c38
Anand MAurya <1705018@kiit.ac.in>
2019-07-04 02:36:01 +0530

    fixed

--- includes/header-post.php
+++
@@ -1,9 +0,0 @@
-<div class="jumbotron">
-    <div class="container">
-      <div id="details" class="animated fadeInLeft">
-        <h1>Welcome in <span>Anand Blogs Site</span></h1>
-        <p>We are glad to be with us!!</p>
-    </div>
-    <!-- <img src="images/slide-7.jpg" alt="Header image"> -->
-  </div>
-</div>
